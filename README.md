637c7ad4d5af9ff68fbf302b43e15923b6e915d9
fastbyt3 <aldeerayan@gmail.com>
2024-01-15 22:09:07 +0530

    exit code same as process

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ mydocker run ubuntu:latest /usr/local/bin/docker-explorer echo hey
 
 - [Rust STDIO ref](https://doc.rust-lang.org/std/process/struct.Stdio.html)
 
+## Handle exit codes
 
+- if program exits with code `1` => our program should exit with code 1
--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,15 @@ fn main() -> Result<()> {
             )
         })?;
 
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        print!("{std_out}");
-        let std_err = std::str::from_utf8(&output.stderr)?;
-        eprint!("{}", std_err);
-    } else {
-        std::process::exit(1);
-    }
-
-    Ok(())
+    let std_out = std::str::from_utf8(&output.stdout)?;
+    print!("{std_out}");
+    let std_err = std::str::from_utf8(&output.stderr)?;
+    eprint!("{}", std_err);
+    std::process::exit(
+        *(&output
+            .status
+            .code()
+            .context("Attempt to read Exit code from process")
+            .unwrap()),
+    );
 }
