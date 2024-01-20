e42f1b0a1b32a9df11fd548ab4d681473e02ad15
fastbyt3 <aldeerayan@gmail.com>
2024-01-20 11:20:25 +0530

    filesystem isolation -> chroot

--- README.md
+++ README.md
@@ -13,3 +13,27 @@ mydocker run ubuntu:latest /usr/local/bin/docker-explorer echo hey
 ## Handle exit codes
 
 - if program exits with code `1` => our program should exit with code 1
+
+## Filesystem isolation
+
+- Using [`chroot`](https://en.wikipedia.org/wiki/Chroot) ensure program doesnt have access to host files
+- create an empty dir and `chroot` into it (also copy binary)
+- [Rust ref: fs::chroot](https://doc.rust-lang.org/std/os/unix/fs/fn.chroot.html)
+
+```rust
+fs::chroot("/sandbox")?;
+std::env::set_current_dir("/")?;
+// continue working in sandbox
+```
+
+- we also need to copy over the binary to the new temp folder which will be the ROOT for the child proc
+
+- NOTE: Docker has replaced CHROOT with PIVOT-ROOT for [security reasons](https://tbhaxor.com/pivot-root-vs-chroot-for-containers/)
+- using pivot-root ref:
+	- https://gist.github.com/penumbra23/df65aaf3d5807c85b62b05608a8f30bd
+    - https://docs.rs/nix/latest/nix/unistd/fn.pivot_root.html
+
+### Pivot root
+
+- given a new root and subdir of current root pivot-root moves root(of child process) to subdir and mounts that as new root point
+- then we unmount the old root and leave the newly created root mount point
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::process::Stdio;
+use std::{env, fs, os::unix::fs::chroot, path::Path, process::Stdio};
 
 use anyhow::{Context, Result};
 
@@ -6,7 +6,31 @@ fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+
+    // chrooting
+    // create temp dir (with /dev/null)
+    // chroot to tmpdir & set curr dir (chdir) to "/"
+    // copy cmd binary (either preserve path / put in root dir)
+    // run cmd
+    let tmpdir = tempfile::tempdir().context("Getting a temp dir").unwrap();
+    let tmpdir_dev = tmpdir.path().join("dev");
+    let tmpdir_dev_null = tmpdir_dev.join("null");
+    fs::create_dir(tmpdir_dev)
+        .context("Creating temp dir with dev dir")
+        .unwrap();
+    fs::write(tmpdir_dev_null, b"")
+        .context("Writing empty dev/null file")
+        .unwrap();
+
+    let cmd_bin = Path::new(&command)
+        .file_name()
+        .context("Get file name of command binary")?;
+    fs::copy(command, tmpdir.path().join(&cmd_bin)).context("Copying cmd bin to tmp dir")?;
+
+    chroot(&tmpdir).context("Chrooting to tmp dir").unwrap();
+    env::set_current_dir("/").context("Setting curr dir (chdir) to tmp root")?;
+
+    let output = std::process::Command::new(Path::new("/").join(&cmd_bin))
         .args(command_args)
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
